b81cc2abc0c29552cd8b21cefc1522f91ff29959
sucoDeGoiaba <flp.pxxs@gmail.com>
2022-04-03 09:48:48 -0300

    A- Arrays e seus métodos

--- index.js
+++ index.js
@@ -78,5 +78,78 @@ const cloneClothes = {...clothes}
 // De modo que fique igual aos objetos pants, shirts e socks
 
 const cloneClothes2 = {shoes : {colors: ['yellow', 'purple']}, ...clothes}
-console.log(cloneClothes2);
+// console.log(cloneClothes2);
 
+// Arrays e seus métodos
+// A partir da variável abaixo e utilizando um método de array, retorne a raiz quadrada de
+// todos os valores do array original em um novo array
+
+var numbers = [4, 9, 16, 25];
+
+function sqrRoot(valores){
+    return valores.map(Math.sqrt)
+}
+// console.log(sqrRoot(numbers));
+
+// A partir da variável abaixo e utilizando um método de array, multiplique todos os valores
+// do array por 10. Utilize arrow function.
+var numbers = [65, 44, 12, 4];
+
+const multiplicando = numbers.map((num)=>{
+    return num * 10
+})
+// console.log(multiplicando);
+
+// A partir da variável abaixo retorne um array com valores de age(idade) maiores ou iguais a
+// 18 com arrow function e filter
+var ages = [32, 33, 16, 40];
+
+const filtro = ages.filter((idade)=> idade >= 18)
+// console.log(filtro);
+
+// A partir do objeto abaixo, retorne somente os nomes dos objetos do tipo cachorro ( type:
+// dog)com filter
+const data = [
+    {
+        name: 'Butters',
+        age: 3,
+        type: 'dog',
+    },
+    {
+        name: 'Lizzy',
+        age: 6,
+        type: 'dog',
+    },
+    {
+        name: 'Red',
+        age: 1,
+        type: 'cat',
+    },
+    {
+        name: 'Joey',
+        age: 3,
+        type: 'dog',
+    },
+];
+
+function filtrandoDogs(obj){
+    if (obj.type === 'dog') {
+        return obj
+    } 
+}
+
+const dogFiltrados = data.filter(filtrandoDogs)
+console.log(dogFiltrados);
+
+// Por fim, ainda utilizando o array acima, retorne a soma da idade de todos os cachorros (
+// type: dog)
+
+
+let contador = 0
+    
+for (let i = 0; i < dogFiltrados.length; i++) {
+    const element = dogFiltrados[i];
+    contador += element.age
+}
+
+console.log(contador);
